ebe70ec48b11483311ba6e739c56e68e09062d15
dfloresdev <hello@dflores.dev>
2020-03-18 08:53:11 -0600

    [Menu] menu ready

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,28 +1,48 @@
 import React from "react";
 import { rhythm } from "../utils/typography";
-import '../styles/components/layout.css';
+import "../styles/components/app.css";
 
 const Layout = ({ children }) => {
   return (
-    <div
-      style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
-        maxWidth: rhythm(24),
-        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-      }}
-    >
-      <header>
-        <a href="/">
-          <img src="https://arepa.s3.amazonaws.com/Platzi-master-logo.png" alt="Platzi Master" />
-        </a>
-      </header>
-      <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()} <a href="https://platzi.com">Platzi Master</a>
-      </footer>
+    <div className="container">
+      <div className="container__aligned">
+        <nav className="menu">
+          <a href="/">
+            <img
+              src="https://arepa.s3.amazonaws.com/Platzi-master-logo.png"
+              alt="Platzi Master"
+            />
+          </a>
+          <a className="menu__item" href="https://platzi.com" target="_blank">
+            <p>Platzi</p>
+          </a>
+          <a className="menu__item" href="#" target="_blank">
+            <p>Team</p>
+          </a>
+          <a className="menu__item" href="#" target="_blank">
+            <p>Meses gratis</p>
+          </a>
+        </nav>
+      </div>
+      <div className="hero"></div>
+      <div>
+        <div
+          style={{
+            marginLeft: `auto`,
+            marginRight: `auto`,
+            maxWidth: rhythm(24),
+            padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          }}
+        >
+          <main>{children}</main>
+          <footer>
+            © {new Date().getFullYear()}{" "}
+            <a href="https://platzi.com">Platzi Master</a>
+          </footer>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Layout;
