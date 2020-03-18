4a33a2e542ccb817262ea6ccb2ba3feabbcb9cfb
dfloresdev <hello@dflores.dev>
2020-03-18 11:26:28 -0600

    [SECTION] hero ready

--- src/components/layout.js
+++ src/components/layout.js
@@ -24,22 +24,41 @@ const Layout = ({ children }) => {
           </a>
         </nav>
       </div>
-      <div className="hero"></div>
-      <div>
-        <div
-          style={{
-            marginLeft: `auto`,
-            marginRight: `auto`,
-            maxWidth: rhythm(24),
-            padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}{" "}
-            <a href="https://platzi.com">Platzi Master</a>
-          </footer>
+      <div className="container__aligned">
+        <div className="hero">
+          <div className="hero__text">
+            <h1>Blog Platzi Master</h1>
+            <p>Un blog hecho por la comunidad para la comunidad 😀</p>
+            <p className="hero__text--style">
+              "If we grow together, we grow stronger"
+            </p>
+          </div>
+          <div>
+            <p className="card-latest-post__title">Último post:</p>
+            <div className="card-latest-post">
+              <div className="card-latest-post__card">
+                <div className="card-latest-post__card--img"></div>
+                <div className="card-latest-post__card--info">
+                  <p className="category">Categoría</p>
+                  <p className="title-post">Un post increíble</p>
+                  <p className="description-post">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Donec feugiat non ligula vel viverra.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
+      <div className="container__aligned">
+        <main>{children}</main>
+      </div>
+      <div className="container__aligned">
+        <footer>
+          © {new Date().getFullYear()}{" "}
+          <a href="https://platzi.com">Platzi Master</a>
+        </footer>
       </div>
     </div>
   );
