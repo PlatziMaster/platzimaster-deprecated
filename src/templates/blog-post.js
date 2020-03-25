774c6c926ed2eb864e23ad0af3147f1b491c1e59
Tonalli López <aguilablanca_7@live.com>
2020-03-24 23:54:03 -0600

    [Feature] Share button

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import gravatar from '../utils/gravatar';
 import '../styles/templates/blog-post.css';
 import { Link, graphql } from "gatsby";
@@ -7,11 +7,42 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 import { rhythm } from "../utils/typography";
 
+import shareIcon from "../../assets/shareIcon.png";
+import twitterIcon from "../../assets/twitterIcon.svg";
+import facebookIcon from "../../assets/facebookIcon.png";
+import copylinkIcon from "../../assets/copylinkIcon.png";
+
 const BlogPostTemplate = ({ data, pageContext, location }) => {
+  const [modal, setModal] = useState(false);
+
   const post = data.markdownRemark;
   const siteTitle = data.site.siteMetadata.title;
   const { previous, next } = pageContext;
   const { title, date, description, author, email, platziUser } = post.frontmatter;
+  const url_post = window.location.href;
+
+  const handleShare = () => {
+    if (navigator.share !== undefined) {
+      navigator.share({
+        title: title,
+        text: description,
+        url: url_post
+      })
+        .then(() => console.log('Compartido'))
+        .catch(error => console.log('Error al compartir', error));
+    } else {
+      setModal(!modal);
+    }
+  };
+
+  const copyLink = () => {
+    url_post.toString();
+    navigator.clipboard.writeText(url_post).then(function () {
+      alert(`Enlace: ${url_post} Copiado`);
+    }, function () {
+      alert("Error al copiar enlace");
+    });
+  };
 
   return (
     <Layout location={location} title={siteTitle}>
@@ -54,7 +85,46 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
         <footer>
         </footer>
       </article>
-
+      <div className="blogpost__share">
+        <button onClick={handleShare}>
+          <img src={shareIcon} alt="Share Icon" />
+          <p>Compartir</p>
+        </button>
+        {
+          modal &&
+          <>
+            <div className="blogpost__share-modal">
+              <a
+                href={`https://twitter.com/intent/tweet?url=${url_post}&text=${title}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                data-show-count="false"
+              >
+                <img
+                  src={twitterIcon}
+                  alt="Twitter"
+                />
+              </a>
+              <a
+                href={`https://www.facebook.com/sharer/sharer.php?u=${url_post}&display=popup`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={facebookIcon}
+                  alt="Facebook"
+                />
+              </a>
+              <img
+                src={copylinkIcon}
+                alt="Copy Link"
+                onClick={copyLink}
+              />
+            </div>
+            <span className="blogpost__share-modalsquare"></span>
+          </>
+        }
+      </div>
       <nav>
         <ul
           style={{
